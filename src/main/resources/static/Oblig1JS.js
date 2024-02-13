0be2fa6fcdf15762b76ebbc1b7485b95d00c0265
nceci <cengu2389@oslomet.no>
2024-02-13 22:27:14 +0100

    js1

--- src/main/resources/static/Oblig1JS.js
+++ src/main/resources/static/Oblig1JS.js
@@ -1,19 +1,20 @@
 let billetter = [];
-function kjopBillett(){
-    let film = document.getElementById("velgFilm").value;
+
+function checkInput(){
     let innAntall = document.getElementById("antall").value;
     let fornavn = document.getElementById("fornavn").value;
     let etternavn = document.getElementById("etternavn").value;
     let telefonnr = document.getElementById("tlf").value;
     let epost = document.getElementById("epost").value;
     let antall = Number(innAntall);
-    //let telefonnr = Number(tlf);
+    let errorCount = 0
 
     //få rød feilmelding ved mangel på input
     if (antall === 0 || antall == null){
         let antallfeil = document.getElementById("feilAntall");
         antallfeil.innerText = "Må skrive noe inn i antall";
         antallfeil.style.color="red";
+        errorCount++
     }
     else {
         document.getElementById("feilAntall").innerText ="";
@@ -22,6 +23,7 @@ function kjopBillett(){
         let fornavnfeil = document.getElementById("feilFornavn");
         fornavnfeil.innerText= "Må skrive noe inn i fornavnet";
         fornavnfeil.style.color="red";
+        errorCount++
     }
     else{ document.getElementById("feilFornavn").innerText ="";
     }
@@ -29,6 +31,7 @@ function kjopBillett(){
         let etternavnfeil = document.getElementById("feilEtternavn");
         etternavnfeil.innerText="Må skrive noe inn i etternavnet";
         etternavnfeil.style.color="red";
+        errorCount++
     }
     else {
         document.getElementById("feilEtternavn").innerText="";
@@ -37,6 +40,7 @@ function kjopBillett(){
         let tlfFeil = document.getElementById("feilTlf");
         tlfFeil.innerText="Må skrive noe inn i telefonnr";
         tlfFeil.style.color="red";
+        errorCount++
     }
     else {
         document.getElementById("feilTlf").innerText="";
@@ -45,10 +49,29 @@ function kjopBillett(){
         let epostfeil = document.getElementById("feilEpost");
         epostfeil.innerText="Må skrive noe inn i eposten";
         epostfeil.style.color="red";
+        errorCount++
     }
     else {
         document.getElementById("feilEpost").innerText="";
     }
+    console.log(errorCount)
+    if (errorCount > 0){
+        return true
+    }else{
+    return false
+    }
+}
+function kjopBillett(){
+    console.log("starter kjøp")
+    let film = document.getElementById("velgFilm").value;
+    let innAntall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value;
+    let telefonnr = document.getElementById("tlf").value;
+    let epost = document.getElementById("epost").value;
+    let antall = Number(innAntall);
+
+    //let telefonnr = Number(tlf);
     const bestill = {
         film : film,
         antall : innAntall,
@@ -57,6 +80,8 @@ function kjopBillett(){
         telefonnr : telefonnr,
         epost : epost
     };
+    console.log(checkInput())
+    if (checkInput() === false){
     billetter.push(bestill);
     let ut ="<table><tr>"+
         "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" +
@@ -67,20 +92,24 @@ function kjopBillett(){
             "</td><td>" + b.epost + "</td>";
         ut += "</tr>"
     }
+        //nullstiller inputtboksene:
+        document.getElementById("velgFilm").value="";
+        document.getElementById("antall").value="";
+        document.getElementById("fornavn").value="";
+        document.getElementById("etternavn").value="";
+        document.getElementById("tlf").value="";
+        document.getElementById("epost").value="";
+        document.getElementById("skrivUt").innerHTML=ut;
 
-    //nullstiller inputtboksene:
-    document.getElementById("velgFilm").value="";
-    document.getElementById("antall").value="";
-    document.getElementById("fornavn").value="";
-    document.getElementById("etternavn").value="";
-    document.getElementById("tlf").value="";
-    document.getElementById("epost").value="";
-
-    document.getElementById("skrivUt").innerHTML=ut;
+    }
+    console.log("Kjøp ferdig")
 }
 
 function slettBillett(){
-    billetter =[];
-    console.log(billetter); //kan se på loggen at det er slettet.
-    kjopBillett();
+    console.log("Biletter: " + billetter); //kan se på loggen Bilettene før sletting.
+    billetter = [];
+    console.log("Biletter slettet: " + billetter); //kan se på loggen at det er slettet.
+    document.getElementById("skrivUt").innerHTML="";
+
+
 }
